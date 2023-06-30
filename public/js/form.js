c0c6ad57a3a271b984631d4361176c7016a6ede4
Nguyễn văn Huy <93319143+Ca-mega3-151@users.noreply.github.com>
2023-07-01 06:36:53 +0700

    paa

--- public/js/form.js
+++ public/js/form.js
@@ -66,46 +66,48 @@ formSign.addEventListener("submit", function(e) {
 
 // đăng kí
 
-// $(document).ready(function () {
-//     $(".form-register").submit(function (event) {
-//         event.preventDefault(); // Ngăn chặn form submit
-//         // Lấy giá trị từ các trường input
-//         const username = $(".username").val();
-//         const password = $(".password").val();
-//         const confirmPassword = $(".resetpassword").val();
-//         const phone = $(".numberphone").val();
-
-
-//         // Kiểm tra tên đăng nhập
-//         const usernamePattern = /^[a-zA-Z][a-zA-Z0-9_]{3,15}$/;
-//         if (!usernamePattern.test(username)) {
-//             displayErrorMessage("Tên đăng nhập không hợp lệ. Tên đăng nhập chỉ chứa chữ cái, chữ số và dấu gạch dưới, bắt đầu bằng chữ cái và có độ dài từ 4 đến 16 ký tự.");
-//             return;
-//         }
-
-//         // Kiểm tra mật khẩu
-//         const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-//         if (!passwordPattern.test(password)) {
-//             displayErrorMessage("Mật khẩu không hợp lệ. Mật khẩu phải chứa ít nhất 8 ký tự, gồm chữ hoa, chữ thường và số.");
-//             return;
-//         }
-
-
-
-//         // Kiểm tra xác nhận mật khẩu
-//         if (password !== confirmPassword) {
-//             displayErrorMessage("Xác nhận mật khẩu không khớp.");
-//             return;
-//         }
-
-//         // Kiểm tra số điện thoại
-//         const phonePattern = /^\d{10}$/;
-//         if (!phonePattern.test(phone)) {
-//             displayErrorMessage("Số điện thoại không hợp lệ. Số điện thoại phải gồm 10 chữ số.");
-//             return
-//         }
-//     }
-
-//     )
-// }
-// )
+$(document).ready(function() {
+    $(".form-register").submit(function(event) {
+      event.preventDefault(); // Ngăn chặn form submit
+      // Lấy giá trị từ các trường input
+      const username = $(".username").val();
+      const password = $(".password").val();
+      const confirmPassword = $(".resetpassword").val();
+      const phone = $(".numberphone").val();
+  
+      // Kiểm tra tên đăng nhập
+      const usernamePattern = /^[a-zA-Z][a-zA-Z0-9_]{3,15}$/;
+      if (!usernamePattern.test(username)) {
+        displayErrorMessage("Tên đăng nhập không hợp lệ. Tên đăng nhập chỉ chứa chữ cái, chữ số và dấu gạch dưới, bắt đầu bằng chữ cái và có độ dài từ 4 đến 16 ký tự.");
+        return;
+      }
+  
+      // Kiểm tra mật khẩu
+      const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+      if (!passwordPattern.test(password)) {
+        displayErrorMessage("Mật khẩu không hợp lệ. Mật khẩu phải chứa ít nhất 8 ký tự, gồm chữ hoa, chữ thường và số.");
+        return;
+      }
+  
+      // Kiểm tra xác nhận mật khẩu
+      if (password !== confirmPassword) {
+        displayErrorMessage("Xác nhận mật khẩu không khớp.");
+        return;
+      }
+  
+      // Kiểm tra số điện thoại
+      const phonePattern = /^\d{10}$/;
+      if (!phonePattern.test(phone)) {
+        displayErrorMessage("Số điện thoại không hợp lệ. Số điện thoại phải gồm 10 chữ số.");
+        return;
+      }
+  
+      // Nếu không có lỗi, tiến hành xử lý đăng ký
+      // ...
+    });
+  });
+  
+  function displayErrorMessage(message) {
+    $(".error-message").text(message);
+  }
+  
